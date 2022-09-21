a4fa36eaf3ab02ca95e90a5f725d3766d5bbef8a
Matthew Jilk <matthewjilk@gmail.com>
2022-09-20 20:01:22 -0500

    not null
    made all columns NOT NULL

--- create-tables.sql
+++ create-tables.sql
@@ -4,25 +4,25 @@ create database ts;
 use ts;
 
 create table `student` (
-	`id` int auto_increment,
-	`fname` varchar(50),
-	`lname` varchar(50),
-	`username` varchar(20),
-	`password` varchar(20),
+	`id` int auto_increment NOT NULL,
+	`fname` varchar(50) NOT NULL,
+	`lname` varchar(50) NOT NULL,
+	`username` varchar(20) NOT NULL,
+	`password` varchar(20) NOT NULL,
 
 	primary key(`id`)
 );
 
 create table `class` (
-	`id` int auto_increment,
-	`name` varchar(50),
+	`id` int auto_increment NOT NULL,
+	`name` varchar(50) NOT NULL,
 
 	primary key(`id`)
 );
 
 create table `enrolled in class` (
-	`student id` int,
-	`class id` int,
+	`student id` int NOT NULL,
+	`class id` int NOT NULL,
 
 	primary key(`student id`, `class id`),
 
@@ -31,16 +31,16 @@ create table `enrolled in class` (
 );
 
 create table `lesson` (
-	`id` int auto_increment,
+	`id` int auto_increment NOT NULL,
 	-- name of lesson, for example: counting, comparisons, addition
-	`name` varchar(50),
-	`class id` int,
+	`name` varchar(50) NOT NULL,
+	`class id` int NOT NULL,
 	-- if false, a new problem will be randomly generated after an incorrect answer is submitted
-	`allow multiple attempts` boolean,
+	`allow multiple attempts` boolean NOT NULL,
 	-- identify the rules for how to generate random problems, number of choices for answer, how to display problem description, etc. For now, we can just have all the problems in a lesson be of the same type.
-	`problem type` varchar(10),
+	`problem type` varchar(10) NOT NULL,
 	-- number of problems that need to be solved to complete the lesson
-	`number of problems` tinyint,
+	`number of problems` tinyint NOT NULL,
 
 	primary key(`id`),
 
@@ -48,8 +48,8 @@ create table `lesson` (
 );
 
 create table `prereq lesson` (
-	`prereq lesson id` int,
-	`lesson id` int,
+	`prereq lesson id` int NOT NULL,
+	`lesson id` int NOT NULL,
 
 	primary key(`prereq lesson id`, `lesson id`),
 
@@ -58,10 +58,10 @@ create table `prereq lesson` (
 );
 
 create table `lesson progress` (
-	`student id` int,
-	`lesson id` int,
+	`student id` int NOT NULL,
+	`lesson id` int NOT NULL,
 	-- number of problems currently solved
-	`progress` tinyint,
+	`progress` tinyint NOT NULL,
 
 	primary key(`student id`, `lesson id`),
 
